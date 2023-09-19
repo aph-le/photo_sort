d2802eb302a6b771b26e17a37199f90190637a85
aph-le <alexanderphilipp@gmx.net>
2023-09-19 21:56:44 +0200

    formated with black

--- photo_sort.py
+++ photo_sort.py
@@ -19,6 +19,7 @@ from alive_progress import alive_bar as progress_bar
 @dataclass
 class PhotoSortConfig:
     """Class to hold the configuration and lambdas."""
+
     src_root_path: Path = Path(".").absolute()
     dst_root_path: Path = Path(".").absolute()
     duplicates_path: Path = Path("./duplicates")
@@ -32,32 +33,56 @@ def parse_arguments() -> PhotoSortConfig:
     parser = argparse.ArgumentParser()
     parser.add_argument("src_root_dir", type=str, help="source root directory")
     parser.add_argument("dst_root_dir", type=str, help="destination root directory")
-    parser.add_argument("-c", "--copy", action="store_true", default=False, help="uses copy instead move for files")
-    parser.add_argument("-d", "--duplicates-folder", type=str, default="duplicates", help="alternative folder name to save duplicate pictures in destination root")
-    parser.add_argument("-r", "--rename-format", type=str, default="%Y-%m-%d_%H-%M-%S", help="format secifier for renaming the file, see python datetime")
+    parser.add_argument(
+        "-c",
+        "--copy",
+        action="store_true",
+        default=False,
+        help="uses copy instead move for files",
+    )
+    parser.add_argument(
+        "-d",
+        "--duplicates-folder",
+        type=str,
+        default="duplicates",
+        help="alternative folder name to save duplicate pictures in destination root",
+    )
+    parser.add_argument(
+        "-r",
+        "--rename-format",
+        type=str,
+        default="%Y-%m-%d_%H-%M-%S",
+        help="format secifier for renaming the file, see python datetime",
+    )
     args = parser.parse_args()
 
     photo_sort_config = PhotoSortConfig()
     photo_sort_config.src_root_path = Path(args.src_root_dir).absolute()
     photo_sort_config.dst_root_path = Path(args.dst_root_dir).absolute()
-    photo_sort_config.duplicates_path = photo_sort_config.dst_root_path / args.duplicates_folder
+    photo_sort_config.duplicates_path = (
+        photo_sort_config.dst_root_path / args.duplicates_folder
+    )
     photo_sort_config.file_rename_format = args.rename_format
 
     if args.copy is True:
-        photo_sort_config.copy_func = lambda file_in, file_out: shutil.copy2(file_in, file_out)
+        photo_sort_config.copy_func = lambda file_in, file_out: shutil.copy2(
+            file_in, file_out
+        )
     else:
-        photo_sort_config.copy_func = lambda file_in, file_out: shutil.move(file_in, file_out)
+        photo_sort_config.copy_func = lambda file_in, file_out: shutil.move(
+            file_in, file_out
+        )
 
     return photo_sort_config
 
 
 def parse_exif_date(date_str: str) -> datetime:
     """Parses the given string to create and return a datetime element.
-    
+
     :param str date_str: contains descrition of date and time
     """
     # todo check for diffrent input strings
-    date_obj = datetime.strptime(date_str,"%Y:%m:%d %H:%M:%S")
+    date_obj = datetime.strptime(date_str, "%Y:%m:%d %H:%M:%S")
 
     return date_obj
 
@@ -65,7 +90,7 @@ def parse_exif_date(date_str: str) -> datetime:
 def safe_copy(config: PhotoSortConfig, src_file: str, dest_file: str):
     """Copy or Move file safely by checking if destination file exist and append -X if case
     is already exists.
-    
+
     :param PhotoSortConfig config: Config which contains the actual copy method.
     :param str src_file: source including complete path.
     :param str dst_file: desired destination file name.
@@ -79,9 +104,13 @@ def safe_copy(config: PhotoSortConfig, src_file: str, dest_file: str):
         dest_file_ext = dest_file_path.suffix
         dest_file_base = dest_file_path.stem
         dest_file_add = 1
-        while Path.joinpath(dest_file_dir, f"{dest_file_base}({dest_file_add}){dest_file_ext}").exists():
+        while Path.joinpath(
+            dest_file_dir, f"{dest_file_base}({dest_file_add}){dest_file_ext}"
+        ).exists():
             dest_file_add = dest_file_add + 1
-        dest_file_path = Path.joinpath(dest_file_dir, f"{dest_file_base}({dest_file_add}){dest_file_ext}")
+        dest_file_path = Path.joinpath(
+            dest_file_dir, f"{dest_file_base}({dest_file_add}){dest_file_ext}"
+        )
         config.copy_func(src_file, str(dest_file_path))
 
 
@@ -114,17 +143,17 @@ def create_pic_folder(config: PhotoSortConfig, date: datetime) -> str:
 
 def create_pic_name(config: PhotoSortConfig, file_name: str, date: datetime) -> str:
     """Create a filename for the copy file according to the configuration.
-    
+
     :param PhotoSortConfig config: Config which contains the actual copy method.
     :param str file_name: old file name include full path
-    :param datetime date: date as base element for the new name 
+    :param datetime date: date as base element for the new name
     """
     file_name_path = Path(file_name)
     file_path = file_name_path.parent
     file_ext = file_name_path.suffix
 
     new_name = date.strftime(config.file_rename_format)
-    file_renamed = str(Path.joinpath(file_path,f"{new_name}{file_ext}"))
+    file_renamed = str(Path.joinpath(file_path, f"{new_name}{file_ext}"))
 
     return file_renamed
 
@@ -136,17 +165,17 @@ def check_unique_file(config: PhotoSortConfig, file, dir) -> str:
         unique = getattr(check_unique_file, "unique_" + dir)
         for path in Path(dir).rglob("*"):
             if path.is_file() is True and path.suffix in config.file_type_list:
-                filehash = hashlib.md5(open(str(path),'rb').read()).hexdigest()
+                filehash = hashlib.md5(open(str(path), "rb").read()).hexdigest()
                 if filehash not in unique:
                     unique[filehash] = str(path.name)
                     setattr(check_unique_file, "unique_" + dir, unique)
     unique = getattr(check_unique_file, "unique_" + dir)
-    filehash = hashlib.md5(open(file, 'rb').read()).hexdigest()
+    filehash = hashlib.md5(open(file, "rb").read()).hexdigest()
     if filehash not in unique:
         filename = str(Path(file).name)
         unique[filehash] = filename
         setattr(check_unique_file, "unique_" + dir, unique)
-        return dir + '/' + filename
+        return dir + "/" + filename
     else:
         return str(config.duplicates_path / Path(file).name)
 
@@ -155,21 +184,20 @@ def main():
     """Main function"""
     config = parse_arguments()
 
-    root_path = config.src_root_path 
-    print('Root Path: ' + str(root_path))
+    root_path = config.src_root_path
+    print("Root Path: " + str(root_path))
 
     with progress_bar(len(list(root_path.rglob("*")))) as prog_bar:
-
         for path in root_path.rglob("*"):
             if path.is_file() is True and path.suffix in config.file_type_list:
                 filename = str(path)
                 prog_bar.text(f"Item: {filename}")
                 picture_meta_dict = get_exif(filename)
                 picture_date_time = ""
-                if 'DateTime' in picture_meta_dict:
-                    picture_date_time = picture_meta_dict['DateTime']
-                if 'DateTimeOriginal' in picture_meta_dict:
-                    picture_date_time = picture_meta_dict['DateTimeOriginal']
+                if "DateTime" in picture_meta_dict:
+                    picture_date_time = picture_meta_dict["DateTime"]
+                if "DateTimeOriginal" in picture_meta_dict:
+                    picture_date_time = picture_meta_dict["DateTimeOriginal"]
                 else:
                     pass
                 if picture_date_time != "":
@@ -182,8 +210,7 @@ def main():
             prog_bar()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger("alive_progress")
     main()
-
